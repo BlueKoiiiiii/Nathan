651e861326d12bd7ad4af7211577c2bc25d975fc
BlueKoiiiiii <danielxie0713@gmail.com>
2023-09-02 13:32:32 -0400

    asd

--- bullet.gd
+++ bullet.gd
@@ -5,3 +5,10 @@ var hitted = false
 func _on_hitbox_body_entered(body):
 	$Bulletanim.play("explosion")
 	print("hit")
+
+
+func _on_hitbox_area_entered(area):
+	Bullet = get_node("Bullet")
+	$Bulletanim.play("explosion")
+	print("hit")
+	Bullet.set_sleeping(true)
