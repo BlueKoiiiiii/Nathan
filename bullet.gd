3694eca75ece81847d1b7ea6a2e2462d8fd2cf7b
BlueKoiiiiii <danielxie0713@gmail.com>
2023-09-02 13:37:13 -0400

    Update bullet.gd

--- bullet.gd
+++ bullet.gd
@@ -1,14 +1,11 @@
 extends RigidBody2D
 var hitted = false
-
+@onready var Bullet = get_node("Bullet")
 
 func _on_hitbox_body_entered(body):
 	$Bulletanim.play("explosion")
-	print("hit")
 
 
 func _on_hitbox_area_entered(area):
-	Bullet = get_node("Bullet")
-	$Bulletanim.play("explosion")
-	print("hit")
-	Bullet.set_sleeping(true)
+	Bullet.visible = false
+	
