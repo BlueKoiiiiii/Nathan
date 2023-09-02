da0105c02dfe902d81d4b27a4a0b4bce7e42c57f
Yohello1 <mp2702737@gmail.com>
2023-09-01 23:22:50 -0400

    Actually started multiplayer

--- Scenes/multiplayer.gd
+++ Scenes/multiplayer.gd
@@ -1,11 +1,49 @@
 extends Node
 
+const PORT = 1002
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# get_tree().paused = true
+	multiplayer.server_relay = false
+	if DisplayServer.get_name() == "headless":
+		print("Automatically starting dedicated server.")
+		_on_host_pressed.call_deferred()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_host_pressed():
+	print("uwu")
+	get_tree().paused = true
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_server(PORT)
+	if peer.get_connection_status() == MultiplayerPeer.CONNECTION_DISCONNECTED:
+		OS.alert("Failed tos tart multi player", "ERROR")
+		return
+	multiplayer.multiplayer_peer = peer
+	
+
+
+func _on_connect_pressed():
+	# Start as client.
+	get_tree().paused = true
+	var txt : String = $MultiUi/ip.text
+	if txt == "":
+		OS.alert("Need a remote to connect to.")
+		return
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_client(txt, PORT)
+	if peer.get_connection_status() == MultiplayerPeer.CONNECTION_DISCONNECTED:
+		OS.alert("Failed to start multiplayer client.")
+		return
+	multiplayer.multiplayer_peer = peer
+	start_game()
+	
+
+func start_game():
+	$Nathan.hide()
+	get_tree().paused = false
+
